4a0f69454bf5f7d1edc8b82e8dfd7783dc872809
Udhayakumar C <udhayaas97@gmail.com>
2020-11-26 21:10:52 +0530

    index removed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const outId = "#ctl00_BodyContentPlaceHolder_navMarkOut";
 async function mark(id) {
     // Browser Setup
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: ['--no-sandbox']
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1024, height: 768 });
